9fe2f1a5e3f98cf4d4af090331ec6e0c98e1ff10
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2022-12-17 21:22:19 +0700

    Code phân trang và đồng bộ hoá url

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,42 +1,47 @@
 import classNames from 'classnames'
+import { Link, createSearchParams } from 'react-router-dom'
+
+import path from 'src/constants/path'
+import { QueryConfig } from 'src/pages/ProductList/ProductList'
 
 interface Props {
-  page: number
-  setPage: React.Dispatch<React.SetStateAction<number>>
+  queryConfig: QueryConfig
   pageSize: number
 }
 
 const RANGE = 2
 
-export default function Pagination({ page, setPage, pageSize }: Props) {
-  let dotBefore = false
-  let dotAfter = false
-
-  const renderDotBefore = (index: number) => {
-    if (!dotBefore) {
-      dotBefore = true
-      return (
-        <button key={index} className='shadown-sm mx-2 rounded border bg-white px-3 py-2'>
-          ...
-        </button>
-      )
-    }
-    return null
-  }
-
-  const renderDotAfter = (index: number) => {
-    if (!dotAfter) {
-      dotAfter = true
-      return (
-        <button key={index} className='shadown-sm mx-2 rounded border bg-white px-3 py-2'>
-          ...
-        </button>
-      )
-    }
-    return null
-  }
+export default function Pagination({ queryConfig, pageSize }: Props) {
+  const page = Number(queryConfig.page)
 
   const renderPagination = () => {
+    let dotBefore = false
+    let dotAfter = false
+
+    const renderDotBefore = (index: number) => {
+      if (!dotBefore) {
+        dotBefore = true
+        return (
+          <span key={index} className='shadown-sm mx-2 rounded border bg-white px-3 py-2'>
+            ...
+          </span>
+        )
+      }
+      return null
+    }
+
+    const renderDotAfter = (index: number) => {
+      if (!dotAfter) {
+        dotAfter = true
+        return (
+          <span key={index} className='shadown-sm mx-2 rounded border bg-white px-3 py-2'>
+            ...
+          </span>
+        )
+      }
+      return null
+    }
+
     return Array(pageSize)
       .fill(0)
       .map((_, index) => {
@@ -53,24 +58,60 @@ export default function Pagination({ page, setPage, pageSize }: Props) {
           return renderDotBefore(index)
         }
         return (
-          <button
+          <Link
+            to={{
+              pathname: path.home,
+              search: createSearchParams({
+                ...queryConfig,
+                page: pageNumber.toString()
+              }).toString()
+            }}
             key={index}
             className={classNames('shadown-sm mx-2 cursor-pointer rounded border bg-white px-3 py-2', {
               'border-cyan-500': pageNumber == page,
               'border-transparent': pageNumber !== page
             })}
-            onClick={() => setPage(pageNumber)}
           >
             {pageNumber}
-          </button>
+          </Link>
         )
       })
   }
   return (
     <div className='mt-6 flex flex-wrap justify-center'>
-      <button className='shadown-sm mx-2 cursor-pointer rounded border bg-white px-3 py-2'>Prev</button>
+      {page === 1 ? (
+        <span className='shadown-sm mx-2 cursor-not-allowed rounded border bg-white/60 px-3 py-2'>Prev</span>
+      ) : (
+        <Link
+          to={{
+            pathname: path.home,
+            search: createSearchParams({
+              ...queryConfig,
+              page: (page - 1).toString()
+            }).toString()
+          }}
+          className='shadown-sm mx-2 cursor-pointer rounded border bg-white px-3 py-2'
+        >
+          Prev
+        </Link>
+      )}
       {renderPagination()}
-      <button className='shadown-sm mx-2 cursor-pointer rounded border bg-white px-3 py-2'>Next</button>
+      {page === pageSize ? (
+        <span className='shadown-sm mx-2 cursor-not-allowed rounded border bg-white/60 px-3 py-2'>Next</span>
+      ) : (
+        <Link
+          to={{
+            pathname: path.home,
+            search: createSearchParams({
+              ...queryConfig,
+              page: (page + 1).toString()
+            }).toString()
+          }}
+          className='shadown-sm mx-2 cursor-pointer rounded border bg-white px-3 py-2'
+        >
+          Next
+        </Link>
+      )}
     </div>
   )
 }
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
+import { omitBy, isUndefined } from 'lodash'
 
 import productApi from 'src/apis/product.api'
 import Pagination from 'src/components/Pagination'
@@ -7,39 +7,59 @@ import useQueryParams from 'src/hooks/useQueryParams'
 import AsideFilter from './AsideFilter'
 import Product from './Product/Product'
 import SortProductList from './SortProductList'
+import { ProductListConfig } from 'src/types/product.type'
+
+export type QueryConfig = {
+  [key in keyof ProductListConfig]: string
+}
 
 const ProductList = () => {
   const queryParams = useQueryParams()
-  const [page, setPage] = useState(1)
+  const queryConfig: QueryConfig = omitBy(
+    {
+      page: queryParams.page || '1',
+      limit: queryParams.limit || '5',
+      sort_by: queryParams.sort_by,
+      exclude: queryParams.exclude,
+      name: queryParams.name,
+      order: queryParams.order,
+      price_max: queryParams.price_max,
+      price_min: queryParams.price_min,
+      rating_filter: queryParams.rating_filter
+    },
+    isUndefined
+  )
 
   const { data } = useQuery({
-    queryKey: ['products', queryParams],
+    queryKey: ['products', queryConfig],
     queryFn: () => {
-      return productApi.getProducts(queryParams)
-    }
+      return productApi.getProducts(queryConfig as ProductListConfig)
+    },
+    keepPreviousData: true
   })
   console.log(data?.data.data)
 
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
-        <div className='grid grid-cols-12 gap-6'>
-          <div className='col-span-3'>
-            <AsideFilter />
-          </div>
-          <div className='col-span-9'>
-            <SortProductList />
-            <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-              {data &&
-                data.data.data.products.map((product) => (
+        {data && (
+          <div className='grid grid-cols-12 gap-6'>
+            <div className='col-span-3'>
+              <AsideFilter />
+            </div>
+            <div className='col-span-9'>
+              <SortProductList />
+              <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+                {data.data.data.products.map((product) => (
                   <div className='col-span-1' key={product._id}>
                     <Product product={product} />
                   </div>
                 ))}
+              </div>
+              <Pagination queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size} />
             </div>
-            <Pagination page={page} setPage={setPage} pageSize={8} />
           </div>
-        </div>
+        )}
       </div>
     </div>
   )
