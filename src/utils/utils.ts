9ab79afcc3a56f51c93cd8ceb3c37b8c23c246ae
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2024-02-25 20:07:50 +0700

    Hiển thị product và format number với Intl

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,3 +8,17 @@ export const isAxiosError = (error: unknown): error is AxiosError => {
 export const isErrorUnprocessableEntity = <T>(error: unknown): error is AxiosError<T> => {
   return isAxiosError(error) && error.response?.status === HttpStatusCode.UnprocessableEntity
 }
+
+export function formatCurrency(currency: number) {
+  return new Intl.NumberFormat('de-DE').format(currency)
+}
+
+export function formatNumberToSocialStyle(value: number) {
+  return new Intl.NumberFormat('en', {
+    notation: 'compact',
+    maximumFractionDigits: 1
+  })
+    .format(value)
+    .replace('.', ',')
+    .toLowerCase()
+}
