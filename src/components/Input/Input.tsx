7880c32635a82c59e18b0ccc30e03956a346ce93
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2024-02-25 16:05:10 +0700

    Code UI AsideFilter

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,7 +3,9 @@ import { UseFormRegister, RegisterOptions } from 'react-hook-form'
 
 interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
   errorMessage?: string
-  register: UseFormRegister<any>
+  classNameInput?: string
+  classNameError?: string
+  register?: UseFormRegister<any>
   rules?: RegisterOptions
 }
 
@@ -15,18 +17,22 @@ export default function Input({
   className,
   errorMessage,
   register,
-  rules
+  rules,
+  classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm',
+  classNameError = 'mt-1 text-red-600 min-h-[1.25rem] text-sm'
 }: InputProps) {
+  const registerResult = register && name ? register(name, rules) : {}
+
   return (
     <div className={className}>
       <input
         type={type}
-        className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+        className={classNameInput}
         placeholder={placeholder}
         autoComplete={autoComplete}
-        {...register(name as string, rules)}
+        {...registerResult}
       />
-      <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errorMessage}</div>
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   )
 }
