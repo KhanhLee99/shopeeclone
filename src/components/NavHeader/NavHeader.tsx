5ae97a84d09b9818c0d8d79e3ec8327485c52f0e
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2024-03-06 17:57:32 +0700

    History Purchase

--- src/components/NavHeader/NavHeader.tsx
+++ src/components/NavHeader/NavHeader.tsx
@@ -77,7 +77,7 @@ export default function NavHeader() {
                 Tài khoản của tôi
               </Link>
               <Link
-                to={URLs.cart}
+                to={URLs.historyPurchase}
                 className='block w-full bg-white py-3 px-4 text-left hover:bg-slate-100 hover:text-cyan-500'
               >
                 Đơn mua
