00159d74654ebc845005a0c19d9b2210f06a2973
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2024-03-07 13:04:54 +0700

    Add vitest

---
+++ src/utils/__tests__/utils.test.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect } from 'vitest'
+import { AxiosError } from 'axios'
+
+import { isAxiosError, isErrorUnprocessableEntity } from '../utils'
+import HttpStatusCode from 'src/constants/httpStatus'
+
+// describe dùng để mô tả tập hợp các ngữ cảnh
+// hoặc 1 đơn vị cần test: Ví dụ function, component
+describe('isAxiosError', () => {
+  // it dùng để ghi chú trường hợp cần test
+  it('isAxiosError func return boolean', () => {
+    expect(isAxiosError(new AxiosError())).toBe(true)
+    expect(isAxiosError(new Error())).toBe(false)
+  })
+})
+
+describe('isErrorUnprocessableEntity', () => {
+  it('isErrorUnprocessableEntity func return boolean', () => {
+    expect(
+      isErrorUnprocessableEntity(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: HttpStatusCode.UnprocessableEntity
+        } as any)
+      )
+    ).toBe(true)
+    expect(
+      isErrorUnprocessableEntity(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: HttpStatusCode.Unauthorized
+        } as any)
+      )
+    ).toBe(false)
+  })
+})
