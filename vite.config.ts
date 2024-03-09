c9be8587729014ed1fd1adbe3944c4e0a46769b2
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2024-03-09 20:54:19 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/test',
+  base: '/test',
   plugins: [react()] as any,
   test: {
     environment: 'jsdom'
