0a67856a0fcb507ba5144f5b0319e755d7d13618
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2024-03-12 23:47:32 +0700

    update: deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test',
+  base: '/shopeeclone',
   plugins: [react()] as any,
   test: {
     environment: 'jsdom',
