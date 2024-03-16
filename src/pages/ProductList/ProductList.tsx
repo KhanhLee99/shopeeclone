c1c0f2248bff5cb45ece876ecf713b498a2f988f
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2024-03-16 09:07:06 +0700

    update: deploy.yml

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -30,7 +30,7 @@ export default function ProductList() {
   const { t } = useTranslation()
   const queryConfig = useQueryConfig()
 
-  const [watchMode, setWatchMode] = useState<WatchModeType>(WatchMode.scroll)
+  const [watchMode, setWatchMode] = useState<WatchModeType>(WatchMode.pagination)
   const [pageSize, setPageSize] = useState(0)
 
   const productsQuery =
